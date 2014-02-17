3b88abea0a5f9b127e046614e26b934a099e5ec5
Carla Cummins <cc21@sanger.ac.uk>
2014-02-17 17:43:09 +0000

    Patterns edits

--- lib/Bio/TIGER/Pattern.pm
+++ lib/Bio/TIGER/Pattern.pm
@@ -14,17 +14,18 @@ use Data::Dumper;
 use Array::Utils;
 
 has 'site_list' => ( is => 'rw', isa => 'ArrayRef', required => 1 );
-has 'patterns' => ( is => 'rw', isa => 'ArrayRef', lazy_build => 1 );
+has 'patterns' => ( is => 'rw', isa => 'HashRef', lazy_build => 1 );
 has 'unknowns' => ( is => 'rw', isa => 'ArrayRef', required => 1);
 
 sub _build_patterns {
 	my ( $self ) = @_;
 	
-	my @pats;
+	my %pats;
 	foreach my $s ( @{ $self->site_list } ){
-		push(@pats, $self->_site_pattern($s));
+	        my @sp = $self->_site_pattern($s);
+		$pats{} += 1;
 	}
-	return \@pats;
+	return \%pats;
 }
 
 sub _site_pattern {
@@ -35,7 +36,8 @@ sub _site_pattern {
 	my (%bases, @order);
 	foreach my $i ( 0 .. $#s ){
 		my $base = $s[$i];
-		unless($base ~~ @unknowns){
+		#unless($base ~~ @unknowns){
+		unless( ++$index until $unknowns[$index] == $want or $index > $#a ){
 		    push( @{ $bases{$base} }, $i );
 		    push( @order, $base ) unless ( $base ~~ @order );
 	    }
@@ -45,8 +47,11 @@ sub _site_pattern {
 	foreach my $k ( @order ){
 		push(@pattern, $bases{$k});
 	}
-	return \@pattern;
+	print Dumper \@pattern;
+	return @pattern;
 }
+
+
 
 no Moose;
 __PACKAGE__->meta->make_immutable;
--- t/Bio/TIGER/Pattern.t
+++ t/Bio/TIGER/Pattern.t
@@ -7,49 +7,31 @@ use Data::Dumper;
 BEGIN { unshift( @INC, './lib' ) }
 BEGIN {
     use Test::Most;
+    no warnings 'experimental::smartmatch';
 }
 
 my (@site_list, @exp_pats, @unknowns, $pats);
 
 use_ok('Bio::TIGER::Pattern');
 
-@site_list = qw( AAAA ATTA CGGT );
-@exp_pats = (
-	[ 
-		[0,1,2,3]
-	],
-	[
-		[0,3],
-		[1,2]
-	],
-	[
-		[0],
-		[1,2],
-		[3]
-	]
-);
+@site_list = qw( AAAA ATTA CGGT GGGG );
+@exp_pats = {
+        "0,1,2,3" => 2,
+	"0,3|1,2" => 1,
+	"0|1,2|3" => 1
+};
 @unknowns = [];
 
 $pats = Bio::TIGER::Pattern->new( site_list => \@site_list, unknowns => \@unknowns )->patterns;
+print Dumper $pats;
 is_deeply $pats, \@exp_pats, 'correct patterns returned';
 
 @site_list = qw( AAAA-TTT ATTA--GG CGGT--GG );
-@exp_pats = (
-	[ 
-		[0,1,2,3],
-		[5,6,7]
-	],
-	[
-		[0,3],
-		[1,2],
-		[6,7]
-	],
-	[
-		[0],
-		[1,2,6,7],
-		[3]
-	]
-);
+@exp_pats = {
+	"0,1,2,3|5,6,7" => 1,
+	"0,3|1,2|6,7" => 1,
+	"0|1,2,6,7|3" => 1
+};
 @unknowns = ['-'];
 
 $pats = Bio::TIGER::Pattern->new( site_list => \@site_list, unknowns => \@unknowns )->patterns;
